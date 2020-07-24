7b1838196ea5ab487cde21fcbb38a01dee13c79c
Oludamilola Osiname <osinameoludamilola@gmail.com>
2020-07-24 18:57:50 +0200

    design header

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <h2 className="world">Where in the world?</h2>
+      <span>Dark Mode</span>
+    </header>
+  );
+};
+
+export default Header;
