dcc4d6359bc09d4e77562222424217524becdcd4
Oludamilola Osiname <osinameoludamilola@gmail.com>
2020-08-02 20:42:19 +0200

    add scss

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 import Main from "./components/Main.js";
 import axios from "axios";
 
-import "./App.css";
+import "./App.scss";
 
 class App extends Component {
   state = {
