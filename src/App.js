6f5ccb4034cb37f7806f12e16ba2824a70593ae3
Oludamilola Osiname <osinameoludamilola@gmail.com>
2020-08-23 15:20:34 +0200

    change state component to functional component

--- src/App.js
+++ src/App.js
@@ -6,49 +6,11 @@ import axios from "axios";
 import "./App.scss";
 
 class App extends Component {
-  state = {
-    countries: [],
-  };
-
-  componentDidMount() {
-    axios.get(`https://restcountries.eu/rest/v2/all`).then((res) => {
-      const countries = res.data;
-      this.setState({ countries: countries });
-    });
-  }
-
-  handleChange = (e) => {
-    const { name, value } = e.target;
-    this.setState({ [name]: value });
-
-    axios.get(`https://restcountries.eu/rest/v2/name/${value}`).then((res) => {
-      const countries = res.data;
-      this.setState({ countries: countries });
-    });
-  };
-
-  filterByRegion = (e) => {
-    const { value } = e.target;
-
-    axios
-      .get(`https://restcountries.eu/rest/v2/region/${value}`)
-      .then((res) => {
-        const countries = res.data;
-        this.setState({ countries: countries });
-      });
-  };
-
   render() {
     return (
       <Fragment>
         <Header />
-        <Main
-          countries={this.state.countries}
-          country={this.state.country}
-          handleChange={this.handleChange}
-          region={this.state.region}
-          filterByRegion={this.filterByRegion}
-        />
+        <Main />
       </Fragment>
     );
   }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import CountryCard from "./CountryCard";
 
 // import countries from "../fakeData";
 
-const Main = (props) => {
+const Main = () => {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    axios.get(`https://restcountries.eu/rest/v2/all`).then((res) => {
+      const countries = res.data;
+      setCountries(countries);
+    });
+  }, []);
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+
+    axios.get(`https://restcountries.eu/rest/v2/name/${value}`).then((res) => {
+      const countries = res.data;
+      setCountries(countries);
+    });
+  };
+
+  const filterByRegion = (e) => {
+    const { value } = e.target;
+
+    axios
+      .get(`https://restcountries.eu/rest/v2/region/${value}`)
+      .then((res) => {
+        const countries = res.data;
+        setCountries(countries);
+      });
+  };
+
   const displayCountries = () => {
-    return props.countries.map((country) => {
+    return countries.map((country) => {
       return <CountryCard key={country.name} country={country} />;
     });
   };
@@ -16,21 +46,21 @@ const Main = (props) => {
           type="text"
           name="country"
           placeholder="Search for a country ..."
-          onChange={props.handleChange}
+          onChange={handleChange}
           className="inputText--country"
         />
 
         <select
           name="region"
-          onChange={props.filterByRegion}
+          onChange={filterByRegion}
           className="inputText--region "
         >
           <option>Filter by region</option>
-          <option value="africa">Africa</option>
-          <option value="america">America</option>
-          <option value="asia">Asia</option>
-          <option value="europe">Europe</option>
-          <option value="oceania">Oceania</option>
+          <option value="Africa">Africa</option>
+          <option value="Americas">America</option>
+          <option value="Asia">Asia</option>
+          <option value="Europe">Europe</option>
+          <option value="Oceania">Oceania</option>
         </select>
       </section>
       <section className="countries">{displayCountries()}</section>
